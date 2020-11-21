bb196673e4f52018b1a5511521b9159dc12d6ec9
asg0828 <asg0828@gmail.com>
2020-11-21 17:23:14 +0900

    게시판 작업 초기 상태

---
+++ src/components/board/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  Row,
+  Input
+} from 'antd';
+
+
+const Board = () => {
+
+  return (
+    <div>게시판</div>
+  )
+}
+
+
+export default Board;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
